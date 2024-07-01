ead87e87649d38821ba65111ceb8ed82bec1b2e3
Mosigisi <emmanuel.mosigisi@student.moringaschool.com>
2024-07-01 12:03:27 +0300

    assignment1

---
+++ index.js
@@ -0,0 +1,10 @@
+let marks = 60;
+if (marks >79){console.log ("A");} 
+else if (marks >=60 && marks <=79){
+    console.log("B");}
+else if (marks >=50 && marks <=49){
+    console.log("C");}
+else if (marks >=40 && marks <=49) {
+    console.log("D");}
+else if (marks <=39) {
+    console.log("E");}
